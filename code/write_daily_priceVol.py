ede8a92cb8fbf063b6aedbbf812a9002ed5f29c4
Yancy Zhang <zhangy8665@gmail.com>
2021-07-05 16:12:33 -0400

    add comments and docu

--- code/write_daily_priceVol.py
+++ code/write_daily_priceVol.py
@@ -5,8 +5,8 @@ import time
 import happybase
 from tqdm import tqdm
 
-host = "localhost" 
-sas=saspy.SASsession(cfgname='default')
+host = "localhost"
+sas = saspy.SASsession(cfgname='default')
 
 crsp_split_code = open(path.expanduser("sas_files/crsp_split.sas"), 'r').read()
 ccm_merged_code = open(path.expanduser("sas_files/ccm_merged_tic.sas"), 'r').read()
@@ -17,35 +17,53 @@ sas.submit(ccm_merged_code)['LOG'].split('\n')
 crsp_split = sas.sd2df(table='crsp_split', libref='work')
 ccm_merged = sas.sd2df(table='ccm_merged_tic', libref='work', method="DISK")
 
-# Processed
+# Table Processed
 crsp_split["EXDT"] = crsp_split.EXDT.dt.date
 crsp_split["PERMNO"] = crsp_split.PERMNO.astype(int)
 ccm_merged["datadate"] = ccm_merged.datadate.dt.date
 ccm_merged["permno"] = ccm_merged.permno.astype(int)
 
-def process_facpr_column(df, i, cf="priceVol"): # only for split
+
+# Dump Data
+def process_facpr_column(df, i, cf="priceVol"):  # only for split
+    """
+    process column "facpr" per line
+    """
     row_key = str(df.loc[i]['PERMNO']) + "#" + str(df.loc[i]['EXDT'])
 
     row_value = dict()
     if not pd.isnull(df.loc[i]["FACPR"]):
-        row_value[':'.join((cf, "FACPR"))] = str(format(df.loc[i]["FACPR"],".4f"))
+        row_value[':'.join((cf, "FACPR"))] = str(format(df.loc[i]["FACPR"], ".4f"))
 
-    return row_key,row_value
+    return row_key, row_value
 
-info_col_name = ["cshoc","ret","retx","divd"]
-def process_info_columns(df,i,columns,cf="info"):
+
+info_col_name = ["cshoc", "ret", "retx", "divd"]
+
+
+def process_info_columns(df, i, columns, cf="info"):
+    """
+    process "info" columnsper line
+    """
     row_key = str(df.loc[i]['permno']) + "#" + str(df.loc[i]['datadate'])
 
     row_value = dict()
     for column in columns:
         value = df.loc[i][column]
         if not pd.isnull(value):
-            row_value[':'.join((cf, column))] = str(format(abs(value),".8f")) if (column=="ret" or column=="retx") else str(value) # format(abs(value),".8f"),abs(value)
+            row_value[':'.join((cf, column))] = str(format(abs(value), ".8f")) if (
+                        column == "ret" or column == "retx") else str(value)  # format(abs(value),".8f"),abs(value)
 
-    return row_key,row_value
+    return row_key, row_value
 
-priceVol_col_name = ["prcod","prccd","prchd","prcld","cshtrd"]
-def process_priceVol_columns(df,i,columns,cf="priceVol"):
+
+priceVol_col_name = ["prcod", "prccd", "prchd", "prcld", "cshtrd"]
+
+
+def process_priceVol_columns(df, i, columns, cf="priceVol"):
+    """
+    process "priceVol" columns per line
+    """
     row_key = str(df.loc[i]['permno']) + "#" + str(df.loc[i]['datadate'])
 
     row_value = dict()
@@ -53,58 +71,66 @@ def process_priceVol_columns(df,i,columns,cf="priceVol"):
         value = df.loc[i][column]
         if not pd.isnull(value):
             row_value[':'.join((cf, column))] = str(value)
-    return row_key,row_value
-
-
-
+    return row_key, row_value
 
 
 def write_to_hbase(pool, table_name, crsp_split, ccm_merged, step):
-    priceVol_col_name = ["prcod","prccd","prchd","prcld","cshtrd"]
-    info_col_name = ["cshoc","ret","retx","divd"]
-    
+    """
+    Parameters
+    ----------
+    pool: happybase.ConnectionPool obj
+    table_name: string, table name in hbase database
+    crsp_split: dataframe, generated from SAS
+    ccm_merged: dataframe, generated from SAS
+    step: int, send to database with number of rows per time
+
+    """
+    priceVol_col_name = ["prcod", "prccd", "prchd", "prcld", "cshtrd"]
+    info_col_name = ["cshoc", "ret", "retx", "divd"]
+
     start_time = time.time()
     with pool.connection() as connection:
-        print("list all tables: ", connection.tables())
+        print("List all tables: ", connection.tables())
         table = connection.table(table_name)
         batch = table.batch()
-        
-        # add all column type, hard coding
+
+        # Add all column type, hard coding, all of them are float64
         column_dtype_key = 'column_data_type'
         column_dtype_value = dict()
-        for column in priceVol_col_name+["FACPR"]:
+        for column in priceVol_col_name + ["FACPR"]:
             column_dtype_value[':'.join(("priceVol", column))] = 'float64'
-        
+
         for column in info_col_name:
             column_dtype_value[':'.join(("info", column))] = 'float64'
         batch.put(column_dtype_key, column_dtype_value)
         batch.send()
-        
-        # dump data from crsp_split for column "facpr"
+
+        # Dump data from crsp_split for column "facpr"
         for i in tqdm(range(len(crsp_split))):
-#         for i in range(10):
+            #         for i in range(10):
             row_key, row_value = process_facpr_column(crsp_split, i, cf="priceVol")
-            
+
             batch.put(row_key, row_value)
-            if (i+1) % step == 0:
+            if (i + 1) % step == 0:
                 batch.send()
-        
-        
-        # dump data from ccm_merged from crsp 
+
+        # Dump data from ccm_merged from crsp merged with ccm table
         for i in tqdm(range(len(ccm_merged))):
-#         for i in range(500):
-            row_key_ccm_info, row_value_ccm_info = process_info_columns(ccm_merged,i,columns=info_col_name,cf="info")
+            #         for i in range(500):
+            row_key_ccm_info, row_value_ccm_info = process_info_columns(ccm_merged, i, columns=info_col_name, cf="info")
             batch.put(row_key_ccm_info, row_value_ccm_info)
-            
-            row_key_ccm_price, row_value_ccm_price = process_priceVol_columns(ccm_merged,i,columns=priceVol_col_name,cf="priceVol")
+
+            row_key_ccm_price, row_value_ccm_price = process_priceVol_columns(ccm_merged, i, columns=priceVol_col_name,
+                                                                              cf="priceVol")
             batch.put(row_key_ccm_price, row_value_ccm_price)
-            
-            if (i+1) % step == 0:
+
+            if (i + 1) % step == 0:
                 batch.send()
-                
+
         batch.send()
-        
-    print("operation takes ", time.time()-start_time)
-            
-pool = happybase.ConnectionPool(size=100,host=host,port=9090,autoconnect=True,timeout=1000000)
-write_to_hbase(pool,'daily_price_vol', crsp_split, ccm_merged, step=100)
+
+    print("operation takes ", time.time() - start_time)
+
+
+pool = happybase.ConnectionPool(size=100, host=host, port=9090, autoconnect=True, timeout=1000000)
+write_to_hbase(pool, 'daily_price_vol', crsp_split, ccm_merged, step=100)
--- code/write_tenq.py
+++ code/write_tenq.py
@@ -1,65 +1,105 @@
 import subprocess
 import time
 import happybase
-import pandas as pd
-import numpy as np
 # https://happybase.readthedocs.io/en/latest/user.html
 # https://github.com/lyveng/pandas-hbase/blob/master/pdhbase/__init__.py
 
 from pymongo import MongoClient
-import pandas as pd
-import html2text
 from tqdm import tqdm
-import concurrent.futures
+
 # wrds-rd thread pool size 300 is optimal
-client = MongoClient('mongodb://localhost:27888',maxPoolSize=300)
+client = MongoClient('mongodb://localhost:27888', maxPoolSize=300)
 db = client['10-Q_parsed']['May2021Algo']
 
+
 def write_to_hbase(pool, mongo_db, table_name, step, cf="cf1"):
+    """
+
+    Parameters
+    ----------
+    pool, happybase.ConnectionPool obj
+    mongo_db, mongoDB obj
+    table_name, string, table name in hbase database
+    step, int, send to database with number of rows per time
+    cf, string, column name for data to store
+
+    """
     start_time = time.time()
     with pool.connection() as connection:
         print("list all tables: ", connection.tables())
         table = connection.table(table_name)
         batch = table.batch()
-        
-#         # create a row for column type
-#         column_dtype_key = 'column_data_type'
-#         column_dtype_value = dict()
-#         for column in df.columns:
-#             column_dtype_value[':'.join((cf, column))] = df.dtypes[column].name
-#         batch.put(column_dtype_key, column_dtype_value)
-        
+
+        #         # create a row for column type
+        #         column_dtype_key = 'column_data_type'
+        #         column_dtype_value = dict()
+        #         for column in df.columns:
+        #             column_dtype_value[':'.join((cf, column))] = df.dtypes[column].name
+        #         batch.put(column_dtype_key, column_dtype_value)
+
+        # list out all records in a iterator
         records = mongo_db.find()
-        
+
         def read_from_mongo():
             line = records.next()
-            row_key = line["_id"]
+            _row_key = line["_id"]
             del line["_id"]
-            
-            row_value = {':'.join((cf, key)):str(val) for key,val in line.items()}
 
-            return str(row_key), row_value
-        
-        
+            _row_value = {':'.join((cf, key)): str(val) for key, val in line.items()}
+
+            return str(_row_key), _row_value
+
         # dump data
-        for i in tqdm(range(records.count())): # records.count()
-        #         for i in tqdm(range(650)): # records.count()
+        for i in tqdm(range(records.count())):  # records.count()
+            #         for i in tqdm(range(650)): # records.count()
             row_key, row_value = read_from_mongo()
-            
+
             batch.put(row_key, row_value)
-            if (i+1) % step == 0:
+            if (i + 1) % step == 0:
                 try:
                     batch.send()
                 except Exception as e:
                     print(e)
                     print("continue to run")
-        
+
         batch.send()
-        
-    print("operation takes ", time.time()-start_time)
-        
+
+    print("operation takes ", time.time() - start_time)
+
+
 write_thread = 300
 host = "localhost"
-pool = happybase.ConnectionPool(size=write_thread,host=host,port=9090,autoconnect=True,timeout=1000000)
+pool = happybase.ConnectionPool(size=write_thread, host=host, port=9090, autoconnect=True, timeout=1000000)
 
-write_to_hbase(pool, mongo_db = db, table_name = 'tenq', step = 10, cf="cf1")
+write_to_hbase(pool, mongo_db=db, table_name='tenq', step=10, cf="cf1")
+
+
+"""
+Hyper parameter tuning on wrds-rd
+
+6500 data, read_thread = 100
+write_thread = 50
+takes 324s
+
+write_thread = 300
+takes 275s
+
+write_thread = 1000
+takes 282s
+-------------------
+6500 data, write_thread = 300
+read_thread = 100
+takes 275s
+
+read_thread = 300
+takes 247s
+-------------------
+6500 data, read_thread = 300, write_thread = 300
+
+step = 10
+takes 247s
+
+step = 100
+takes 232s
+
+"""
